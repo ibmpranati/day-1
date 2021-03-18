949bfdd9eaae4a407d9a0df2a7d39b05b5aa64ee
unknown <ibmjavanh15@iiht.tech>
2021-03-18 08:30:26 +0530

    File Handling concepts

---
+++ FileHandling/src/ReadFromConsole.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class ReadFromConsole {
+	public static void main(String[] args) {
+		byte[] input = new byte[100];
+		try {
+			System.in.read(input);
+			System.out.println(input);
+		}catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
